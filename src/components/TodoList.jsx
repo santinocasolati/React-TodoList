0abb5fcdb0f79f971e4624831f43c9b00dc597f4
santinocasolati <santicasolati@gmail.com>
2022-06-12 19:43:40 -0300

    Todo List Created (no styling)

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { TodoItem } from './TodoItem'
+
+export function TodoList({ todos, toggleTodo }) {
+    return (
+        <ul>
+            {
+                todos.map((todo) => {
+                    return <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
+                })
+            }
+        </ul>
+    )
+}
